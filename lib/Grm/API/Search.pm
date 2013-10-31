2573eb72130319cd75b2537accd6a2cfe60db828
Ken Youens-Clark <kclark@cshl.edu>
2013-10-31 14:46:22 -0400

    Moving fr

---
+++ lib/Grm/API/Search.pm
@@ -0,0 +1,51 @@
+package Grm::API::Search;
+
+use Mojo::Base 'Mojolicious::Controller';
+use Data::Dumper;
+
+use lib '/usr/local/gramene-lib/lib';
+use Grm::Search;
+use Grm::Utils qw( camel_case commify iterative_search_values timer_calc );
+
+# This action will render a template
+sub search {
+    my $self   = shift;
+    my $search = Grm::Search->new;
+    my $timer  = timer_calc();
+    my $req    = $self->req;
+    my $res    = {};
+
+    if ( my $qry = $req->param('qry') ) {
+        my $format   = $req->param('fmt')      || 'json';
+        my $db       = $req->param('db')       || '';
+        my $category = $req->param('category') || '';
+        my $taxonomy = $req->param('taxonomy') || '';
+
+        for my $t ( iterative_search_values( $qry ) ) {
+            $res         =  $search->search_mysql(
+                query    => $t, 
+                category => $category,
+                taxonomy => $taxonomy,
+                db       => $db,
+            );
+
+            last if $res->{'num_hits'} > 0;
+        }
+
+        $res->{'time'} = $timer->();
+    }
+
+    $self->respond_to(
+        json => sub {
+            $self->render( json => $res );
+        },
+        html => sub { 
+            $self->render( results => $res );
+        },
+        txt => sub { 
+            $self->render( text => Dumper($res) );
+        },
+    );
+}
+
+1;
