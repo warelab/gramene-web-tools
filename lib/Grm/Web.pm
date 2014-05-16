b5ed7b44a5cc3546a2fa218cf9a7834bd94a3d4d
Ken Youens-Clark <kclark@cshl.edu>
2014-05-16 11:00:39 -0400

    new ontology views, redirects for searches

--- lib/Grm/Web.pm
+++ lib/Grm/Web.pm
@@ -1,6 +1,7 @@
 package Grm::Web;
 
 use File::Spec::Functions;
+use Grm::Utils qw( camel_case );
 use Mojo::Base 'Mojolicious';
 use Mojo::Log;
 
@@ -28,17 +29,35 @@ sub startup {
     # Documentation browser under "/perldoc"
     $self->plugin('PODRenderer');
 
+    $self->sessions->default_expiration(86400);
+
     # Router
     my $r = $self->routes;
 
     # Normal route to controller
     $r->get('/')->to('root#home');
+
+    $r->any('/cart/edit')->to('cart#edit');
+
+    $r->any('/cart/download')->to('cart#download');
+
+    $r->any('/cart/count')->to('cart#count');
+
+    $r->post('/cart/empty')->to('cart#empty');
+
+    $r->get('/cart/view')->to('cart#view');
+
+    $r->get('/feedback')->to('feedback#form');
+
+    $r->post('/feedback/submit')->to('feedback#submit');
     
     $r->get('/feedback')->to('feedback#form');
 
     $r->post('/feedback/submit')->to('feedback#submit');
     
     $r->get('/search')->to('search#search');
+
+    $r->get('/search/cloud')->to('search#cloud');
 
     $r->get('/search/log')->to('search#log');
 
@@ -54,15 +73,86 @@ sub startup {
 
     $r->get('/rest/ontology_search')->to('rest#ontology_search');
 
-    $r->get('/rest/ontology_associations/:term_id')->to(
-        'rest#ontology_associations'
+    $r->get('/ontology/association_report/:term_id')->to(
+        'ontology#association_report'
     );
 
+    $r->get('/rest/view_cart')->to('rest#view_cart');
+
     $r->get('/ontology')->to('ontology#search');
+
+    $r->get('/ontology/term/:id')->to('ontology#term');
 
     $r->get('/markers')->to('markers#search');
 
     $r->get('/view/:module/:table/:id')->to('view#object');
+
+    $self->hook(
+        after_dispatch => sub {
+            my $c = shift;
+
+            if ( defined $c->param('download') ) {
+                $c->res->headers->add(
+                    'Content-type' => 'application/force-download' );
+
+                my $file = $c->req->url->path;
+                $file =~ s{.+/}{};
+
+                $c->res->headers->add(
+                    'Content-Disposition' => qq[attachment; filename=$file] );
+            }
+        }
+    );
+
+    $self->helper(make_web_link => sub {
+        my $self      = shift;
+        my %args      = @_;
+        my $link_conf = $args{'link_conf'} || {};
+        my $module    = $args{'module'}    || '';
+        my $table     = $args{'table'}     || '';
+        my $id        = $args{'id'}        || '';
+        my $doc       = $args{'doc'}       || {};
+        my $test      = join '-', $module, $table;
+        my $link_tmpl = '';
+
+        for my $key ( sort keys %$link_conf ) {
+            if ( $test eq $key || $test =~ /$key/ ) {
+                $link_tmpl = $link_conf->{ $key };
+                last;
+            }
+        }
+
+        my $url = '';
+        if ( $link_tmpl =~ /^TT:(.+)/ ) {
+            my $tt_tmpl = $1;
+            my $obj     = {};
+
+            if ( $id ) {
+                my $db      = Grm::DB->new( $module );
+                my $schema  = $db->schema;
+                my $rs_name = camel_case( $table );
+                $obj        = $schema->resultset( $rs_name )->find( $id );
+            }
+
+            my $tt = Template->new;
+
+            $tt->process( 
+                \$tt_tmpl, 
+                { 
+                    object => $obj,
+                    %args
+                }, 
+                \$url 
+            ) or $url = $tt->error;
+        }
+        else {
+            if ( $module && $table && $id ) {
+                $url = "/view/$module/$table/$id";
+            }
+        }
+
+        return $url;
+    });
 }
 
 1;
--- lib/Grm/Web/View.pm
+++ lib/Grm/Web/View.pm
@@ -10,10 +10,25 @@ sub object {
     my $module  = $self->param('module');
     my $table   = $self->param('table');
     my $id      = $self->param('id');
-    my $db      = Grm::DB->new( $module );
-    my $schema  = $db->schema;
-    my $rs_name = camel_case( $table );
-    my $obj     = $schema->resultset( $rs_name )->find( $id );
+    my $config  = $self->config;
+
+    if (my @redirects = @{ $config->{'view'}{'redirect'} || [] }) {
+        my $test = join('-', $module, $table);
+        for my $r (@redirects) {
+            if ($test =~ /$r/) {
+                my $link = $self->make_web_link(
+                    module    => $module,
+                    table     => $table,
+                    id        => $id,
+                    link_conf => $config->{'view'}{'link'},
+                );
+
+                if ($link && $link ne $self->req->url) {
+                    $self->redirect_to($link);
+                }
+            }
+        }
+    }
 
     $self->layout('default');
 
@@ -27,25 +42,29 @@ sub object {
         },
 
         html => sub {
-            my %args = (
-                module => $module,
-                table  => $table,
-                id     => $id,
-                obj    => $obj,
-                db     => $db,
-                schema => $schema,
+            my $db      = Grm::DB->new($module);
+            my $schema  = $db->schema;
+            my $rs_name = camel_case($table);
+            my $obj     = $schema->resultset($rs_name)->find($id);
+            my %args    = (
+                module  => $module,
+                table   => $table,
+                id      => $id,
+                obj     => $obj,
+                db      => $db,
+                schema  => $schema,
             );
 
-            my $conf  = $self->config;
-            my %views = %{ $conf->{'view'}{'template'} || {} };
-            my $test  = join '-', $module, $table;
-
-            for my $key ( %views ) {
-                if ( $test eq $key || $test =~ /$key/ ) {
-                    $args{'template'} = $views{ $key };
-                    last;
-                }
-            }
+#            my $conf  = $self->config;
+#            my %views = %{ $conf->{'view'}{'template'} || {} };
+#            my $test  = join '-', $module, $table;
+#
+#            for my $key ( %views ) {
+#                if ( $test eq $key || $test =~ /$key/ ) {
+#                    $args{'template'} = $views{ $key };
+#                    last;
+#                }
+#            }
 
             $self->render( %args );
         },
